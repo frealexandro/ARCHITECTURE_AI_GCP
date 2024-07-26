96f7d711814e11bf2d5cbd5f262e0b83fdd0050b
frealexandro <jhoner300@gmail.com>
2024-07-26 17:21:43 -0500

    I add the supabase CRUD to the SQL database , but we need to add a NOSQL database to some files ,the to add the two types of database we need to finsh the code that execute the pipeline in gcp with the service account

---
+++ api/supabase_crud.py
@@ -0,0 +1,38 @@
+
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
+
+class ItemCRUD:
+    def __init__(self, supabase_client):
+        self.supabase = supabase_client
+
+    def create_item(self, item: Item):
+        response = self.supabase.table("items").insert(item.dict()).execute()
+        if response.error:
+            raise HTTPException(status_code=400, detail=response.error.message)
+        return response.data[0]
+
+    def read_item(self, item_id: int):
+        response = self.supabase.table("items").select("*").eq("id", item_id).execute()
+        if response.error:
+            raise HTTPException(status_code=400, detail=response.error.message)
+        if not response.data:
+            raise HTTPException(status_code=404, detail="Item not found")
+        return response.data[0]
+
+    def update_item(self, item_id: int, item: Item):
+        response = self.supabase.table("items").update(item.dict()).eq("id", item_id).execute()
+        if response.error:
+            raise HTTPException(status_code=400, detail=response.error.message)
+        return response.data[0]
+
+    def delete_item(self, item_id: int):
+        response = self.supabase.table("items").delete().eq("id", item_id).execute()
+        if response.error:
+            raise HTTPException(status_code=400, detail=response.error.message)
+        return {"message": "Item deleted successfully"}
